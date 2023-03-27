7339ae45713f31398fa74edc3bb4ad4e478a6d9c
DMeehan25 <833056@mystma.org>
2023-03-27 15:13:16 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan( x , y):
+    if x > y:
+        return True
+    else:
+        return False
+def lessThan():  
+    if x < y:
+        return True
+    else:
+        return False
+def equalTo():  
+    if x == y:
+        return True
+    else:
+        return False
+def greaterOrEqual():  
+    if x >= y:
+        return True
+    else:
+        return False
+def lessOrEqual():
+    if x <= y:
+        return True
+    else:
+        return False
